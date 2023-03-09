e88bc2c4598f415c55c4ce461f810ced2812447e
Erik Sjöström <erikdsjostrom@gmail.com>
2023-03-09 19:51:45 +0100

    Factor out Bot

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod database;
 mod error;
 mod player;
 mod utils;
+mod bot;
 
+use bot::Bot;
 use database::Database;
 use serenity::model::prelude::command::Command;
-use utils::{cup_number_from_unixtime, parse_wordle_msg, recalcualate_high_scores};
 
 use std::fmt::Display;
 use std::sync::Arc;
@@ -19,7 +20,6 @@ use error::{Error, Result};
 
 use serenity::async_trait;
 
-use serenity::futures::StreamExt;
 use serenity::model::application::interaction::Interaction;
 use serenity::model::application::interaction::InteractionResponseType;
 use serenity::model::prelude::*;
@@ -32,140 +32,10 @@ use crate::utils::current_cup_number;
 const CHANNEL_ID: u64 = 938727764037619712;
 const GUILD_ID: u64 = 486522741395161108;
 
-struct Bot {
-    database: Arc<Database>,
-}
-
 struct Scoresheet {
     // TODO
 }
 
-// TODO: Factor out
-impl Bot {
-    async fn set_medals(&self, day: i64, channel_id: ChannelId, http: &Context) -> Result<()> {
-        // playerId, msgId, score
-        for (p, medalists) in [
-            (
-                Placement::Gold,
-                self.database.get_gold_medalist(Some(day)).await?,
-            ),
-            (
-                Placement::Silver,
-                self.database.get_silver_medalist(Some(day)).await?,
-            ),
-            (
-                Placement::Bronze,
-                self.database.get_bronze_medalist(Some(day)).await?,
-            ),
-        ] {
-            if medalists.is_none() {
-                continue;
-            }
-            let medalists = medalists.unwrap();
-            for (_, msg_id, _) in medalists {
-                let msg_id = MessageId(msg_id as u64);
-                channel_id
-                    .create_reaction(http, msg_id, ReactionType::Unicode(p.to_string()))
-                    .await?;
-            }
-        }
-        Ok(())
-    }
-
-    async fn clear_medals(&self, day: i64, channel_id: ChannelId, http: &Context) -> Result<()> {
-        // playerId, msgId, score
-        for (p, medalists) in [
-            (
-                Placement::Gold,
-                self.database.get_gold_medalist(Some(day)).await?,
-            ),
-            (
-                Placement::Silver,
-                self.database.get_silver_medalist(Some(day)).await?,
-            ),
-            (
-                Placement::Bronze,
-                self.database.get_bronze_medalist(Some(day)).await?,
-            ),
-        ] {
-            if medalists.is_none() {
-                continue;
-            }
-            let medalists = medalists.unwrap();
-            for (_, msg_id, _) in medalists {
-                let msg_id = MessageId(msg_id as u64);
-                channel_id
-                    .delete_reaction(http, msg_id, None, ReactionType::Unicode(p.to_string()))
-                    .await?;
-            }
-        }
-        Ok(())
-    }
-    async fn new_score_sheet(&self, msg: &Message) -> Result<()> {
-        let cup_number = cup_number_from_unixtime(msg.timestamp.unix_timestamp());
-        let player_id = msg.author.id.0 as i64;
-        let msg_id = msg.id.0 as i64;
-        let score_sheet = msg
-            .content
-            .strip_prefix("Wordle")
-            .ok_or(Error::Message(format!(
-                "new_score_sheet called with a non wordle msg: {}",
-                msg.content
-            )))?;
-        // Create new player if not exists
-        self.database.new_player(player_id).await?;
-        let (day, score) = parse_wordle_msg(score_sheet)?;
-        // TODO: Is there a better place to do this to avoid runtime error if this is not executed first?
-        self.database.new_daily(day).await?;
-        debug!("Day: {}, Score: {}, Cup number: {}", day, score, cup_number);
-        self.new_daily_score(Some(day), score).await?;
-        self.database
-            .new_score_sheet(msg_id, day, player_id, score, cup_number)
-            .await?;
-        Ok(())
-    }
-
-    async fn read_old_messages(&self, channel_id: ChannelId, http: &Context) {
-        debug!("Reading old messages");
-        let mut msg_count: i64 = 0;
-        let mut messages = channel_id.messages_iter(http).boxed();
-        while let Some(msg_result) = messages.next().await {
-            let msg = match msg_result {
-                Err(e) => {
-                    error!("{e}");
-                    return;
-                }
-                Ok(msg) => msg,
-            };
-            if msg.content.starts_with("Wordle") {
-                msg_count = msg_count + 1;
-                _ = self.new_score_sheet(&msg).await;
-            }
-        }
-        info!("Old messages read: {msg_count}");
-    }
-
-    async fn new_daily_score(&self, day: Option<i64>, score: i64) -> Result<()> {
-        if score == 0 {
-            return Ok(());
-        }
-        let day = day.unwrap_or(self.database.get_daily_day().await?);
-        let high_scores = self.database.get_daily_high_scores(day).await?;
-        let high_scores = recalcualate_high_scores(high_scores, score);
-        self.database
-            .update_daily(day, high_scores[0], high_scores[1], high_scores[2])
-            .await
-    }
-
-    async fn handle_wordle_message(&self, msg: Message, ctx: &Context) -> Result<()> {
-        let score_sheet = msg.content.strip_prefix("Wordle").unwrap();
-        let (day, _) = parse_wordle_msg(score_sheet).unwrap();
-        self.clear_medals(day, msg.channel_id, &ctx).await?;
-        self.new_score_sheet(&msg).await?;
-        self.set_medals(day, msg.channel_id, &ctx).await?;
-        Ok(())
-    }
-}
 
 #[derive(Debug, Hash, PartialEq, Eq, Copy, Clone)]
 pub enum Placement {
